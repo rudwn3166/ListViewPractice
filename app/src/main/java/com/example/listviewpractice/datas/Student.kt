b4c47c97ad2a91f5b686c969a6fd90cef0703225
Joo <rudwn3166@naver.com>
2021-09-09 20:29:19 +0900

    학생 데이터를 표현하는 클래스를 추가 하위 정보로는 이름 출생년도 성별

---
+++ app/src/main/java/com/example/listviewpractice/datas/Student.kt
@@ -0,0 +1,11 @@
+package com.example.listviewpractice.datas
+
+//학생 하위정보 -이름,출생년도
+//이름 출생년도 성별 넣은 생성자 생성함
+class Student(
+    val name : String,
+    val birthYear : Int,
+    val gender : String){
+
+
+}
