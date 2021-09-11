7cd80ab66911f9221b22e6a07a47f9a3ec0e9f58
Joo <rudwn3166@naver.com>
2021-09-11 21:40:25 +0900

    학생 목록을 담을 ArrayList생성,실제 목록 추가

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("오경주",1995,"여성"))
+        mStudentList.add(Student("김말수",1989,"남자"))
+        mStudentList.add(Student("라용니",7818,"여성"))
+        mStudentList.add(Student("개소리",2001,"남자"))
+        mStudentList.add(Student("꼬미",2018,"여성"))
+        mStudentList.add(Student("오나",1597,"성별모름"))
+        mStudentList.add(Student("라벨라",1234,"남자"))
     }
 }
