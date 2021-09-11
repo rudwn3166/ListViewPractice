817b0e704b401c54f207e15625b7555bdd94882d
Joo <rudwn3166@naver.com>
2021-09-11 22:13:52 +0900

    각 학생의 이름과 출생년도 성별 반영 -출생년도는 String 가공 기능 재활용

--- app/src/main/java/com/example/listviewpractice/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listviewpractice.R
 import com.example.listviewpractice.datas.Student
 
@@ -21,6 +22,20 @@ class StudentAdapter(val mContext : Context,
         }
 
         val row = tempRow!!
+
+//        위치에 맞는 데이터 가져오기
+        val studentData = mList[position]
+
+//        뷰를 아이디로 찾아달라
+        val nameTxt =row.findViewById<TextView>(R.id.nameTxt)
+        val birthYearTxt = row.findViewById<TextView>(R.id.birthYearTxt)
+        val genderTxt = row.findViewById<TextView>(R.id.genderTxt)
+
+        nameTxt.text = studentData.name
+//        스트링 가공 법 ${변수사용 가능}
+        birthYearTxt.text = "(${studentData.birthYear}년생)"
+        genderTxt.text = studentData.gender
+
         return row
     }
 
